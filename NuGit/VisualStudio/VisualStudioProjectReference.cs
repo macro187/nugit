77738cacddd13f5b1297902c364f2667edd2a1c4
Ron MacNeil <macro@hotmail.com>
2016-10-23 12:55:01 +1000

    Start teaching NuGit about Visual Studio solutions

---
+++ NuGit/VisualStudio/VisualStudioProjectReference.cs
@@ -0,0 +1,66 @@
+﻿using NuGit.Infrastructure;
+
+namespace NuGit.VisualStudio
+{
+
+    /// <summary>
+    /// A Visual Studio solution reference to a project
+    /// </summary>
+    ///
+    /// <remarks>
+    /// References are in <c>.sln</c> files in the following format:
+    /// <code>
+    /// Project("&lt;TypeId&gt;") = "&lt;Name&gt;", "&lt;Location&gt;", "&lt;Id&gt;"
+    /// ...
+    /// EndProject
+    /// </code>
+    /// </remarks>
+    /// 
+    public class VisualStudioProjectReference
+    {
+
+        public VisualStudioProjectReference(
+            string id,
+            string typeId,
+            string name,
+            string location,
+            int lineNumber,
+            int lineCount)
+        {
+            Id = id;
+            TypeId = typeId;
+            Name = name;
+            Location = location;
+            LineNumber = lineNumber;
+            LineCount = lineCount;
+        }
+        
+
+        public string Id { get; private set; }
+
+
+        public string TypeId { get; private set; }
+
+
+        public string Name { get; private set; }
+
+
+        public string Location { get; private set; }
+
+
+        public int LineNumber { get; private set; }
+
+
+        public int LineCount { get; private set; }
+
+
+        public override string ToString()
+        {
+            return StringExtensions.FormatInvariant(
+                "{0}:{1}: Project(\"{2}\") = \"{3}\", \"{4}\", \"{5}\"",
+                LineNumber, LineCount, TypeId, Name, Location, Id);
+        }
+
+    }
+
+}
---
+++ NuGit/VisualStudio/VisualStudioSolution.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace NuGit.VisualStudio
+{
+
+    /// <summary>
+    /// Information in a Visual Studio solution file
+    /// </summary>
+    ///
+    public class VisualStudioSolution
+    {
+
+        /// <summary>
+        /// Initialise a new solution from the lines of text in the <c>.sln</c> file
+        /// </summary>
+        ///
+        public VisualStudioSolution(IEnumerable<string> lines)
+        {
+            if (lines == null) throw new ArgumentNullException("lines");
+            _lines = lines.ToList();
+            Parse();
+        }
+
+
+        /// <summary>
+        /// The lines of text in the file
+        /// </summary>
+        ///
+        public IEnumerable<string> Lines
+        {
+            get { return _lines; }
+        }
+
+        IList<string> _lines;
+
+
+        /// <summary>
+        /// Parse information from file contents
+        /// </summary>
+        ///
+        void Parse()
+        {
+            _projectReferences = new List<VisualStudioProjectReference>();
+
+            int lineNumber = 0;
+            int startLine = 0;
+            string id = "";
+            string typeId = "";
+            string name = "";
+            string location = "";
+            foreach (var line in Lines)
+            {
+                lineNumber++;
+
+                //
+                // In a project reference
+                //
+                if (startLine != 0)
+                {
+                    if (line.Trim() == "EndProject")
+                    {
+                        _projectReferences.Add(
+                            new VisualStudioProjectReference(
+                                id, typeId, name, location, startLine, lineNumber - startLine + 1));
+                        startLine = 0;
+                        id = "";
+                        typeId = "";
+                        name = "";
+                        location = "";
+                    }
+                    continue;
+                }
+
+                //
+                // Starting a project reference
+                //
+                var match = Regex.Match(line, "Project\\(\"([^\"]*)\"\\) = \"([^\"]*)\", \"([^\"]*)\", \"([^\"]*)\"");
+                if (match.Success)
+                {
+                    if (startLine != 0)
+                        throw new VisualStudioParseException(
+                            "Expected 'EndProject'",
+                            lineNumber,
+                            line);
+                    startLine = lineNumber;
+                    typeId = match.Groups[1].Value;
+                    name = match.Groups[2].Value;
+                    location = match.Groups[3].Value;
+                    id = match.Groups[4].Value;
+                    continue;
+                }
+
+                //
+                // Nothing special
+                //
+            }
+        }
+
+
+        public IEnumerable<VisualStudioProjectReference> ProjectReferences
+        {
+            get { return _projectReferences; }
+        }
+
+        IList<VisualStudioProjectReference> _projectReferences;
+
+    }
+
+}
