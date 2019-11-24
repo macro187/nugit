8ba74f33d92e8e0de52cbd17333b9d59bd7f09ed
Ron MacNeil <macro@hotmail.com>
2019-11-24 21:06:18 +1000

    Fix boms and line endings

--- nugit/NuGitRepository.cs
+++ nugit/NuGitRepository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using IOPath = System.IO.Path;
 using System.Collections.Generic;
--- nugit/NuGitWorkspace.cs
+++ nugit/NuGitWorkspace.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using MacroGit;
 using System.Collections.Generic;
